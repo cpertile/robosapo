d04485b4f4f0ce688d1ddb5c52ad64e403e527ea
cpertile <c.pertile@icloud.com>
2019-09-27 01:22:51 -0300

    Criadas funções diferentes para aceleração

--- ROBOSAPO/ControleMotor.ino
+++ ROBOSAPO/ControleMotor.ino
@@ -4,7 +4,7 @@ void PararMotores() {
   analogWrite(PINO_PWM_MOTOR_B, 0);
 }
 
-void AcelerarMotores(int porcentagem_pwm) {
+void AceleracaoConjunta(int porcentagem_pwm) {
   if (porcentagem_pwm < 0 || porcentagem_pwm > 100) {
     Serial.println("Porcentagem inválida (permitido: 0-100)");
     return;
@@ -17,7 +17,7 @@ void AcelerarMotores(int porcentagem_pwm) {
   analogWrite(PINO_PWM_MOTOR_B, pwm_convertido);
 }
 
-void AcelerarGradualmente(int espera) {
+void AceleracaoConjuntaGradual(int espera) {
   int pwm = PWM_MINIMO;
   for (pwm; pwm <= PWM_MAXIMO; pwm += 25)
   {
@@ -27,4 +27,16 @@ void AcelerarGradualmente(int espera) {
     analogWrite(PINO_PWM_MOTOR_B, pwm);
     Espera(espera);
   }
+}
+
+void AceleracaoDiferencial(int pwm_a, int pwm_b) {
+  int pwm_a_convertido = map(pwm_a, 0, 100, PWM_MINIMO, PWM_MAXIMO);
+  int pwm_b_convertido = map(pwm_a, 0, 100, PWM_MINIMO, PWM_MAXIMO);
+  
+  String mensagemA = "Acelerando Motor A: " + String(pwm_a_convertido) + " PWM";
+  String mensagemB = "Acelerando Motor B: " + String(pwm_b_convertido) + " PWM";
+  Serial.println(mensagemA);
+  Serial.println(mensagemB);
+  analogWrite(PINO_PWM_MOTOR_A, pwm_convertido);
+  analogWrite(PINO_PWM_MOTOR_B, pwm_convertido);
 }
