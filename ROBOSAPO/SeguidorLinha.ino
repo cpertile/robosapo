19415d6d3d3f9733b5ec7e4ebf19ae88defd4ed1
cpertile <c.pertile@icloud.com>
2019-10-14 23:32:21 -0300

    Sensores de linha funcionando em conjunto

--- ROBOSAPO/SeguidorLinha.ino
+++ ROBOSAPO/SeguidorLinha.ino
@@ -1,3 +1,43 @@
 /*
  * S.A.P.O. - Seguidor Autônomo de Percurso Orientado
+ * Ordem de leitura dos sensores:
+ *    - Esquerda
+ *    - Centro
+ *    - Direita
  */
+#define NUM_SENSORES 3
+
+void incializarSeguidores() {
+ pinMode(PINO_ANALOGICO_SENSOR_ESQUERDO, INPUT);
+ pinMode(PINO_ANALOGICO_SENSOR_CENTRAL, INPUT);
+ pinMode(PINO_ANALOGICO_SENSOR_DIREITO, INPUT);
+}
+
+bool sensorNaLinha(int leitura) {
+  if (leitura > 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void lerSensores() {  
+  int leituraEsquerdo = analogRead(PINO_ANALOGICO_SENSOR_ESQUERDO);
+  int leituraCentral = analogRead(PINO_ANALOGICO_SENSOR_CENTRAL);
+  int leituraDireito = analogRead(PINO_ANALOGICO_SENSOR_DIREITO);  
+ 
+  bool esquerdoNaLinha = sensorNaLinha(leituraEsquerdo);
+  bool centralNaLinha = sensorNaLinha(leituraCentral);
+  bool direitoNaLinha = sensorNaLinha(leituraDireito);
+
+  String mensagem = "ESQ [";
+  mensagem += esquerdoNaLinha;
+  mensagem += "] | MEI [";
+  mensagem += centralNaLinha;
+  mensagem += "] | DIR [";
+  mensagem += direitoNaLinha;
+  mensagem += "]";
+  Serial.println(mensagem); 
+
+  delay(10);
+}
