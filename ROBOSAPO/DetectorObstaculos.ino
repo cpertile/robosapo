0701213596369beedda1073906926292f1577885
cpertile <c.pertile@icloud.com>
2019-11-16 12:29:24 -0300

    Criadas funções de leitura do detector de obstáculos (ultrassom)

--- ROBOSAPO/DetectorObstaculos.ino
+++ ROBOSAPO/DetectorObstaculos.ino
@@ -4,9 +4,32 @@
 
 #include <HCSR04.h>
 
-UltraSonicDistanceSensor SensorDistancia(PINO_TRIGGER, PINO_ECHO);
+void inicializarSensorDistancia() {
+	UltraSonicDistanceSensor SensorDistancia(PINO_TRIGGER, PINO_ECHO);
+}
 
-void getDistanciaObstaculo() {
-  double distancia = SensorDistancia.measureDistanceCm();
-  Serial.println(distancia);
+float getDistanciaAbsoluta() {
+  float distanciaLida = SensorDistancia.measureDistanceCm();
+  Serial.println("Distancia lida direto: " + String(distancia));
+  return distanciaLida;
+}
+
+float getDistanciaNormalizada() {
+	float distancia1 = getDistanciaAbsoluta();
+	float distancia2 = getDistanciaAbsoluta();
+	float distancia3 = getDistanciaAbsoluta();
+
+	float distanciaNormalizada = (distancia1 + distancia2 + distancia3)/3;
+
+	Serial.println("Distancia normalizada: " + String(distanciaNormalizada));
+	return distanciaNormalizada;
+}
+
+bool detectarObstaculo() {
+	float distancia = getDistanciaNormalizada();
+	if (distancia <= 10) {
+		return true;
+	} else {
+		return false;
+	}
 }
